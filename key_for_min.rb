ecab4faf65a4f402764b2906da5102a06b1832d3
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-23 20:22:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
 smallest_value = 0
 smallest_key = nil
-name_hash.each do |value, key|
+name_hash.each do |key, value|
   if smallest_value == 0 
     smallest_value = value 
    key_for_min_value = key
